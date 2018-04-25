4fc94bf005da14998db9b6b23e72e411756e9367
J.Lyu <jlyu@wpi.edu>
2018-04-24 21:52:29 -0400

    Setup client side (react with graphql) of the application.

--- lyrical-graphql/server/server.js
+++ lyrical-graphql/server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://jinyan:password@ds255889.mlab.com:55889/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
