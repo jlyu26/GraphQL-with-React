d3d3bd9edbfab29208e520bcd840378d1e965aa2
J.Lyu <jlyu@wpi.edu>
2018-04-26 13:13:05 -0400

    Lyrical app finish.

--- lyrical-graphql/client/components/SongList.js
+++ lyrical-graphql/client/components/SongList.js
@@ -7,14 +7,17 @@ import query from '../queries/fetchSongs';
 
 class SongList extends Component {
 	onSongDelete(id) {
-		this.props.mutate({ variables: { id } });
+		this.props.mutate({ variables: { id } })
+			.then(() => this.props.data.refetch());
 	}
 
 	renderSongs() {
 		return this.props.data.songs.map(({ id, title }) => {
 			return (
 				<li key={id} className="collection-item">
-					{title}
+					<Link to={`/songs/${id}`}>
+						{title}
+					</Link>
 					<i className="material-icons" onClick={() => this.onSongDelete(id)}>
 						delete
 					</i>
